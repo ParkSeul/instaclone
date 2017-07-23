1524b8883001f81171d6e279394f8e91061639ce
ParkSeul <psgpooh@naver.com>
2017-07-23 18:05:46 +0900

    overlays fade out!!!! finish
    ye!!!!!!!!!!!!!

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-﻿$(document).ready(function(){
-  var $heart = $(".heart");
+﻿$(document).ready(function() {
+  var $heart = $(".heart"),
+    $ellipsis = $(".fa-ellipsis-h"),
+    $overlay = $(".overlay"),
+    $close = $(".fa-times"),
+    $cancel = $("#cancel");
+
+
   $heart.click(function(){
     //$(this).toggleClass("fa-heart-o 8");
 
@@ -22,6 +28,18 @@
     //$(this).addClass("heart-pumping");
     //console.log("clicked");
   });
+
+   $ellipsis.click(function() {
+    $overlay.fadeIn(500);
+  });
+
+  $close.click(closePopUp);
+  $cancel.click(closePopUp);
+
+  function closePopUp(event) {
+    event.preventDefault();
+    $overlay.fadeOut(500);
+  }
 });
 
 
