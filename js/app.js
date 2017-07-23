7d8f35cb3adb0dce97ab4f0107bbebcd0836feb7
ParkSeul <psgpooh@naver.com>
2017-07-23 16:24:02 +0900

    j-query  minified 복붙
    <script
      src="http://code.jquery.com/jquery-3.2.1.min.js"
      integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
      crossorigin="anonymous"></script>

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-﻿alert("lalalal");
+﻿var weapons =["gun", "cannon", "landmine", "bomb", "sword" ];
+
+var soldier = {
+  name: "Nicolas",
+  age: 57,
+  korean: false,
+  weapon: weapons[2],
+  sayHi: function(name){
+    console.log("Hello" + name);
+  }
+}; 
+
+var age = prompt("How old are you?");
+if(age<18){
+  console.log("gtf");
+}
+else if(age>18 && age <50){
+  console.log("father");
+}
+else if(age>50 && age <80){
+  console.log("grandfather");
+}
+else{
+  console.log("dead");
+}
+
+
+
+function multiply(one, two){
+  return one*two;
+}
+
+function divide(one, two){
+  return one/two;
+}
+
+function add(one, two){
+  return one+two;
+}
+
+function subtraction(one, two){
+  return one-two;
+}
